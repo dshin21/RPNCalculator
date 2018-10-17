dc93f0533af5e5b6dc2b302b3721c97f1a73ccf2
Daniel Shin <danielshin234@gmail.com>
2018-10-17 14:33:29 -0700

    init

---
+++ cmake-build-debug/CMakeFiles/RPNCalculator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RPNCalculator.dir/main.cpp.obj"
+  "RPNCalculator.pdb"
+  "RPNCalculator.exe"
+  "RPNCalculator.exe.manifest"
+  "RPNCalculator.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RPNCalculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
